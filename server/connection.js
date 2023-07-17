17f6276e79886eef5fb4d568a8fdb8f8757bff79
IvanGMtz <navi.garces24@gmail.com>
2023-07-17 11:14:57 -0500

    feat: :sparkles: Update connection.js using environment variables

--- server/connection.js
+++ server/connection.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+let config = process.env;
 
 const con = mysql.createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "123456",
-    database: "centroapoyocampus",
+    host: `${config.DB_HOST}`,
+    user: `${config.DB_USER}`,
+    password: `${config.DB_PASSWORD}`,
+    database: `${config.DB_NAME}`,
     port: 3306
 });
 
