40a79a19511a254f6d88a3ed57d36f341ab159c2
IvanGMtz <navi.garces24@gmail.com>
2023-07-17 10:51:29 -0500

    feat: :sparkles: Update method post of aReview.js router
    If we have the same area, this adds the values ​​of the entered parameters.

--- routers/aReview.js
+++ routers/aReview.js
@@ -4,18 +4,61 @@ const appReview = Router();
 
 
 appReview.post("/", (req, res) => {
+    const { nombre, computadores, teclados, mouse, diademas } = req.body;
     con.query(
-        /*sql*/ `INSERT INTO areas_review SET ?`,
-        req.body,
-        (err,data,fils)=>{
-            console.log(err);
-            data.affectedRows+=200;
-            let result = req.body;
-            result.id= data.insertId;
-            res.status(data.affectedRows).send(result);
+      /*sql*/ `SELECT * FROM areas_review WHERE nombre = ?`,
+      [nombre],
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Error en el servidor");
+        } else {
+          if (data.length === 0) {
+            con.query(
+              /*sql*/ `INSERT INTO areas_review SET ?`,
+              { nombre, computadores, teclados, mouse, diademas },
+              (err, data, fields) => {
+                if (err) {
+                  console.log(err);
+                  res.status(500).send("Error en el servidor");
+                } else {
+                  const result = { id: data.insertId, nombre, computadores, teclados, mouse, diademas };
+                  res.status(200).send(result);
+                }
+              }
+            );
+          } else {
+            const existingArea = data[0];
+            const updatedComputadores = existingArea.computadores + computadores;
+            const updatedTeclados = existingArea.teclados + teclados;
+            const updatedMouse = existingArea.mouse + mouse;
+            const updatedDiademas = existingArea.diademas + diademas;
+  
+            con.query(
+              /*sql*/ `UPDATE areas_review SET computadores = ?, teclados = ?, mouse = ?, diademas = ? WHERE id = ?`,
+              [updatedComputadores, updatedTeclados, updatedMouse, updatedDiademas, existingArea.id],
+              (err, data, fields) => {
+                if (err) {
+                  console.log(err);
+                  res.status(500).send("Error en el servidor");
+                } else {
+                  const result = {
+                    id: existingArea.id,
+                    nombre: existingArea.nombre,
+                    computadores: updatedComputadores,
+                    teclados: updatedTeclados,
+                    mouse: updatedMouse,
+                    diademas: updatedDiademas,
+                  };
+                  res.status(200).send(result);
+                }
+              }
+            );
+          }
         }
+      }
     );
-})
+  });
 
 appReview.get("/", (req, res) => {
     con.query(
